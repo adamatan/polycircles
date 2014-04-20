f4446875777b386ea2a4fe46ed33eaf096d0f3d0
Adam Matan <adam@vioozer.com>
2014-04-20 19:00:21 +0300

    First BDD test passing.

--- features/steps/polycircle_steps.py
+++ features/steps/polycircle_steps.py
@@ -1,11 +1,13 @@
 from polycircles import polycircles
+from nose.tools import assert_almost_equal
+from geographiclib import geodesic
 
 @given('A polycircle class with center at {latitude}, {longitude} with a radius of {radius} meters and {num_vertices} vertices')
 def step_impl(context, latitude, longitude, radius, num_vertices):
-    context.latitude = latitude
-    context.longitude = longitude
-    context.radius = radius
-    context.num_vertices = num_vertices
+    context.latitude = float(latitude)
+    context.longitude = float(longitude)
+    context.radius = float(radius)
+    context.num_vertices = int(num_vertices)
 
 @when('then class is initialized')
 def step_impl(context):
@@ -17,6 +19,17 @@ def step_impl(context):
 
 @then('the distance from the center to each vertex is 100, up to 5 decimal digits')
 def step_impl(context):
-    for vertex in context.polycircle.to_lat_lon():
-        print(vertex)
-        assert False
+    #for vertex in context.polycircle.to_lat_lon():
+    #    print(vertex)
+    #    assert False
+    vertices = context.polycircle.to_lat_lon()
+    for vertex in vertices:
+        vertex_number = vertices.index(vertex)
+        expected_azimuth = 360.0/len(vertices) * vertex_number
+        actual_azimuth = (geodesic.Geodesic.WGS84.Inverse(
+            context.latitude, context.longitude, vertex[0], vertex[1]))['azi1']
+
+        if actual_azimuth < 0:
+            actual_azimuth = 360.0 + actual_azimuth
+
+        assert_almost_equal(expected_azimuth, actual_azimuth, places=5)
