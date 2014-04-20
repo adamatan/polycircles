7a1aaa2a5edcec1129c1a0014c3e745742de3590
Adam Matan <adam@vioozer.com>
2014-04-21 02:02:30 +0300

    Commit all tests.

--- polycircles/test/test_kmls.py
+++ polycircles/test/test_kmls.py
@@ -1,7 +1,7 @@
 import unittest
 from polycircles import polycircles
-from nose.tools import assert_equal, assert_almost_equal, assert_is_not_none
 import simplekml
+import pprint
 
 class TestGeometry(unittest.TestCase):
     """Tests the KML representation of the Polycircles using the simplekml
@@ -23,12 +23,12 @@ class TestGeometry(unittest.TestCase):
 
     def test_kml_polygon_2(self):
         """Creates a bagel-shaped polygon."""
-        outer_polycircle = polycircles.Polycircle(latitude=32.0746,
-                                                  longitude=34.791469,
+        outer_polycircle = polycircles.Polycircle(latitude=32.074523,
+                                                  longitude=34.792,
                                                   radius=20,
                                                   number_of_vertices=36)
-        inner_polycircle = polycircles.Polycircle(latitude=32.0746,
-                                                  longitude=34.791469,
+        inner_polycircle = polycircles.Polycircle(latitude=32.074523,
+                                                  longitude=34.792,
                                                   radius=16,
                                                   number_of_vertices=36)
 
@@ -39,6 +39,67 @@ class TestGeometry(unittest.TestCase):
         pol.style.polystyle.color = \
             simplekml.Color.changealphaint(200, simplekml.Color.red)
         kml.save("test_kml_polygon_2.kml")
+
+    def test_olympic_bagels(self):
+        """Creates bagel-shaped polygons in Rio (just for fun).."""
+        centres = ( (-22.971499,          -43.183030),
+                    (-22.971498999730088, -43.18273744279518),
+                    (-22.97149899892034,  -43.182444885590364)
+        )
+
+
+        outer = {'latitude': -22.97149, 'longitude': -43.183030,
+                 'radius': 20, 'number_of_vertices': 36}
+        outer_polycircle = polycircles.Polycircle(**outer)
+
+        inner = outer.copy()
+        inner["radius"] = 16
+        inner_polycircle = polycircles.Polycircle(**inner)
+        #pprint.pprint(outer_polycircle.to_lat_lon())
+
+        outer2=outer.copy()
+        outer2["longitude"]=-43.18273744279518
+
+
+        kml = simplekml.Kml()
+        pol = kml.newpolygon(name="Azrieli towers",
+                             outerboundaryis=outer_polycircle.to_kml(),
+                             innerboundaryis=inner_polycircle.to_kml())
+
+        pol2 = kml.newpolygon(name="2",
+                             outerboundaryis=polycircles.Polycircle(**outer2).to_kml())
+
+        pol2.style.polystyle.color = \
+            simplekml.Color.changealphaint(150, simplekml.Color.red)
+
+
+        pol.style.polystyle.color = \
+            simplekml.Color.changealphaint(150, simplekml.Color.blue)
+        kml.save("test_olympic_bagels.kml")
+
+    def test_olympic_bagels2(self):
+        """Creates bagel-shaped polygons in Rio (just for fun).."""
+        circles = ((-22.971499, -43.183030, simplekml.Color.blue),
+                    (-22.971498, -43.182737, simplekml.Color.black),
+                    (-22.971498, -43.182444, simplekml.Color.red),
+                    (-22.971666, -43.182892, simplekml.Color.yellow),
+                    (-22.971665, -43.182599, simplekml.Color.green)
+        )
+
+        kml = simplekml.Kml()
+
+        for circle in circles:
+            outer_polycircle = polycircles.Polycircle(circle[0], circle[1],
+                                                      radius=20)
+            inner_polycircle = polycircles.Polycircle(circle[0], circle[1],
+                                                      radius=16)
+            pol = kml.newpolygon(outerboundaryis=outer_polycircle.to_kml(),
+                                 innerboundaryis=inner_polycircle.to_kml())
+            pol.style.polystyle.color = \
+                simplekml.Color.changealphaint(150, circle[2])
+
+        kml.save("test_olympic_bagels_2.kml")
+
 
 
 if __name__ == '__main__':
