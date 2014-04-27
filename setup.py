8a720616a34b5aa782d87bfcabb22506287a192e
Adam Matan <adam@vioozer.com>
2014-04-27 10:22:50 +0300

    Update package classifiers.

--- setup.py
+++ setup.py
@@ -3,6 +3,20 @@ from setuptools import setup
 setup(name='polycircles',
       version='0.1',
       description='Polycircles: WGS84 Circle approximations using polygons',
+      long_description=open('README').read(),
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3.3",
+          "Topic :: Scientific/Engineering :: GIS",
+          "Topic :: Software Development :: Libraries",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Information Technology",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent"
+      ],
       url='https://github.com/adamatan/polycircles',
       author='Adam Matan',
       author_email='adam@matan.name',
@@ -13,3 +27,5 @@ setup(name='polycircles',
       tests_require=['geopy >= 0.99', 'nose >= 1.3.0', 'simplekml >= 1.2.3'],
       test_suite='polycircles.test',
       zip_safe=False)
+from setuptools import setup
+
