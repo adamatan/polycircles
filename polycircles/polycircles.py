a23be2511ff51fcd839b0820dd8959ea6a2f4d9e
John Klapp <57602411+klappj@users.noreply.github.com>
2021-03-31 12:54:39 -0600

    Integrated Adam Matan's input
    corrected variable name   e v.s. self.e
    fixed description

--- polycircles/polycircles.py
+++ polycircles/polycircles.py
@@ -79,11 +79,11 @@ class Polycircle(Shape):
         self.vertices = tuple(vertices)
 
 class Polyellipse(Shape):
-    """A polygonial approximation of a circle in WGS84 coordinates.
+    """A polygonial approximation of an ellipse in WGS84 coordinates.
 
     Usage example:
 
-    >>> c=polycircles.Polyellipse(latitude=31.830039, longitude=35.071912, semmi-major=100, semi-minor = 80, orientation=30)
+    >>> c=polycircles.Polyellipse(latitude=31.830039, longitude=35.071912, semmimajor=100, semiminor = 80, orientation=30)
     >>> c.to_lat_lon()
     ((31.83094084460926, 35.071912), (31.830910114707777, 35.07218540190941), (31.830820019253167, 35.07244017141744), (31.83067669825975, 35.07265894602757), (31.830489919032214, 35.07282681647418), (31.83027241048205, 35.07293234280127), (31.830038995615485, 35.07296833393961), (31.82980558132776, 35.07293233766568), (31.829588074359023, 35.07282680757908), (31.829401297291742, 35.07265893575638), (31.82925797845859, 35.07244016252233), (31.829167884585402, 35.07218539677381), (31.829137155262753, 35.071912), (31.829167884585402, 35.071638603226184), (31.82925797845859, 35.07138383747767), (31.829401297291742, 35.071165064243615), (31.829588074359023, 35.07099719242092), (31.82980558132776, 35.07089166233432), (31.830038995615485, 35.07085566606038), (31.83027241048205, 35.07089165719872), (31.830489919032214, 35.07099718352581), (31.83067669825975, 35.07116505397242), (31.830820019253167, 35.071383828582555), (31.830910114707777, 35.07163859809059))
     >>> c.to_wkt()
@@ -99,7 +99,7 @@ class Polyellipse(Shape):
         longitude -- WGS84 longitude, between -180.0 and 180.0.
         semimajor -- semi-major axis in meters.
         semiminor -- semi-minor axis in meters.
-        orientation -- orientation oof the semi-major axis in degrees.
+        orientation -- orientation of the semi-major axis in degrees.
         number_of_vertices -- Number of vertices for the approximation
         polygon.
         """
@@ -128,7 +128,7 @@ class Polyellipse(Shape):
         vertices = []
         for i in range(number_of_vertices):
             degree = 360.0/number_of_vertices*i # degrees from semimajor axis
-            radius = semiminor/sqrt(1-e*cos(radians(degree)))
+            radius = semiminor/sqrt(1-self.e*cos(radians(degree)))
             angle = degree + orientation # rotate the ellipse to the orientation specified 
             vertex = geodesic.Geodesic.WGS84.Direct(latitude, longitude,
                                                     angle, radius)
