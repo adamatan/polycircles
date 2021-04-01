631bb8a11a3830dece3c2b8070dfc993523524ed
Adam Matan <adam@matan.name>
2021-04-01 11:55:07 +0300

    Use assertions instead of raising exceptions

--- polycircles/test/test_exceptions.py
+++ polycircles/test/test_exceptions.py
@@ -5,51 +5,51 @@ from nose.tools import raises
 class TestExceptions(unittest.TestCase):
     """Tests that the right exceptions are raised for erroneous inputs."""
 
-    @raises(ValueError)
+    @raises(AssertionError)
     def test_less_than_3_vertices_no_1(self):
         polycircle = polycircles.Polycircle(latitude=30,
                                            longitude=30,
                                            radius=100,
                                            number_of_vertices=2)
 
-    @raises(ValueError)
+    @raises(AssertionError)
     def test_less_than_3_vertices_no_2(self):
         polycircle = polycircles.Polycircle(latitude=30,
                                            longitude=30,
                                            radius=100,
                                            number_of_vertices=-3)
 
-    @raises(ValueError)
+    @raises(AssertionError)
     def test_less_than_3_vertices_no_3(self):
         polycircle = polycircles.Polycircle(latitude=30,
                                            longitude=30,
                                            radius=100,
                                            number_of_vertices=0)
 
-    @raises(ValueError)
+    @raises(AssertionError)
     def test_erroneous_latitude_1(self):
         polycircle = polycircles.Polycircle(latitude=-100,
                                             longitude=30,
                                             radius=100)
 
-    @raises(ValueError)
+    @raises(AssertionError)
     def test_erroneous_latitude_2(self):
         polycircle = polycircles.Polycircle(latitude=100,
                                             longitude=30,
                                             radius=100)
 
-    @raises(ValueError)
+    @raises(AssertionError)
     def test_erroneous_latitude_3(self):
         polycircle = polycircles.Polycircle(latitude=200,
                                             longitude=30,
                                             radius=100)
-    @raises(ValueError)
+    @raises(AssertionError)
     def test_erroneous_longitude_1(self):
         polycircle = polycircles.Polycircle(latitude=30,
                                             longitude=-200,
                                             radius=100)
 
-    @raises(ValueError)
+    @raises(AssertionError)
     def test_erroneous_longitude_2(self):
         polycircle = polycircles.Polycircle(latitude=30,
                                             longitude=200,
